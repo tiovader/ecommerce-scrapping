221ebc76d133e7526decb7f2cac58f12e3b924e6
Emanuel Luis <emanuel.filho08@gmail.com>
2022-04-11 18:28:57 -0300

    Running spiders with scrapy API

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 from ecommerce.ecommerce.spiders import SPIDERS
+import os
 
+settings_file_path = 'ecommerce.ecommerce.settings'
+os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_file_path)
 
 configure_logging()
 settings = get_project_settings()
 runner = CrawlerRunner(settings)
 
+
 @defer.inlineCallbacks
 def crawl():
     yield from map(runner.crawl, SPIDERS)
     reactor.stop()
+
 
 crawl()
 reactor.run()
