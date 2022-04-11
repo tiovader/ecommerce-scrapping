b85f5c0ba7e0d877c6ed99db890bcc6cdd61f057
Emanuel Luis <emanuel.filho08@gmail.com>
2022-04-11 17:16:23 -0300

    running spiders with scrapy builtins

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-from ecommerce.src.spiders import SPIDERS
-import os
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-base = f'{dir_path}/venv/Scripts/activate'
-if os.name == 'nt':
-    ACTIVATE_ENV = base + '.bat'
-else:
-    ACTIVATE_ENV = f'source {base}'
-
-CHANGE_DIRECTORY = 'cd ecommerce'
-RUN_SPIDER = 'scrapy crawl {}'
-COMMANDS = ' && '.join((ACTIVATE_ENV, CHANGE_DIRECTORY, RUN_SPIDER))
-
-def main():
-    for spider in SPIDERS:
-        os.system(COMMANDS.format(spider.name))
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+from ecommerce.ecommerce.spiders import SPIDERS
 
 
-if __name__ == '__main__':
-    main()
+configure_logging()
+settings = get_project_settings()
+runner = CrawlerRunner(settings)
+
+@defer.inlineCallbacks
+def crawl():
+    yield from map(runner.crawl, SPIDERS)
+    reactor.stop()
+
+crawl()
+reactor.run()
